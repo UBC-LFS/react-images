8d292d8256e3c89bd49d2cf38f42454889f1e7e3
Patrick Lin <patricklin@alumni.ubc.ca>
2017-07-26 14:09:53 -0700

    remove iscroll

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import myScroll from 'IScroll'
 import { css, StyleSheet } from 'aphrodite/no-important';
 import defaults from '../theme';
 import { deepMerge } from '../utils';
